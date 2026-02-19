7dafaa6e056baaa80dee3d31782aa826ad3f42ed
Gayatri Kulkarni <gaytrikulkarni616@gmail.com>
2026-02-19 21:06:42 +0530

    Create javaBasics.java
    java Basics

---
+++ javaBasics.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class javaBasics{
+  public static void main(String[] args){
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");      
+}
