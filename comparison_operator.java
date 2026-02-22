fd1384cac63d995fadc35d48be00722c3acfde24
Gayatri Kulkarni <gaytrikulkarni616@gmail.com>
2026-02-22 22:15:44 +0530

    Create comparison_operator.java

---
+++ comparison_operator.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class javaBasics{
+    public static void main(String[] args){
+    int a=10;
+    int b=10;
+    System.out.println(a==b);//true
+    System.out.println(a!=b);//false
+
+    int a=10;
+    int b=20;
+    System.out.println(a>=b);//false
+    System.out.println(a<=b);//true
+
+    }
+}
