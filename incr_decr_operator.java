a3b146e0b29d6180fcdc84d91c8223ff5c135bab
Gayatri Kulkarni <gaytrikulkarni616@gmail.com>
2026-02-22 22:13:59 +0530

    Create incr_decr_operator.java

---
+++ incr_decr_operator.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class javaBasics{
+    public static void main(String[] args){
+       int a=10;
+       System.out.println(++a);//11 use
+       System.out.println(a);
+       System.out.println(a++);//11 12
+       System.out.println(a);
+       System.out.println(--a);//12 11 use
+       System.out.println(a);
+       System.out.println(a--);//11 10
+       System.out.println(a);
+ 
+    }
+}
