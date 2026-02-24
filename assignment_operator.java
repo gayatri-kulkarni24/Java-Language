fa7dedce618d759d685fe04876a4223a469b5631
Gayatri Kulkarni <gaytrikulkarni616@gmail.com>
2026-02-24 20:33:46 +0530

    Create assignment_operator.java

---
+++ assignment_operator.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class javaBasics{
+    public static void main(String[] args){
+    // Assignment opertors
+    int A=10;
+    System.out.println(A+=10);
+    System.out.println(A-=10); //A=A-10
+    System.out.println(A*=10);
+    System.out.println(A/=10);
+    System.out.println(A%=10);
+ 
+    }
+}
